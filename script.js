3050fce0b27a2e66a5b28b1c6a3a9875784c0a8b
kindnemo <namanmishraepicstar@gmail.com>
2022-03-08 18:33:23 +0530

    Add the grid change and reset functions
    Added functionalities
    -Grid range slider
    -Grid range slider displays the value on input
    -reset button
    -reset button resets the whole grid (Clears it and sets up a new canvas with the input number of grids)
    
    Added optimization
    -Grid cubes now have slimmer shadow
    -Clear button has pointing cursor and shadow
    -The grid range display div no longer changes its size based on the content inside

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ const drawArea = document.querySelector("#draw-area");
 const opacity = document.querySelector("#opacity");
 const gridColor = document.querySelector("#grid-color");
 const brushColor = document.querySelector("#brush-color");
+const gridNumber = document.querySelector("#grids");
+const rangePara = document.querySelector("#range-para");
+const resetBtn = document.querySelector("#clear-canvas");
 
-// function colorPick{
-
-// }
 
 function hexTorgb(hex) {  //Converts the hex code to rgb value
     return ['0x' + hex[1] + hex[2] | 0, '0x' + hex[3] + hex[4] | 0, '0x' + hex[5] + hex[6] | 0];
@@ -20,8 +20,18 @@ function Color(){
     this.style.background = `rgba(${red},${green},${blue}, ${colorOpacity})`;
 }
 
+function displayGridNumber(){
+    let grids = gridNumber.value;
+    rangePara.textContent = `${grids}x${grids}`;
+}
 
+
+function reset(){       //Resets the whole board and sets up a new canvas
+    let grids = parseInt(gridNumber.value);
+    addDiv(grids);
+}
 function addDiv(num){
+    drawArea.innerHTML = '';
     drawArea.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
     drawArea.style.gridTemplateRows = `repeat(${num}, 1fr)`;
 
@@ -35,3 +45,5 @@ function addDiv(num){
 }
 
 window.onload = addDiv(16);   //Puts 16x16 grids on website load
+gridNumber.addEventListener("input", displayGridNumber);
+resetBtn.addEventListener("click", reset);
